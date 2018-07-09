36a2f167793b6f4ba73521f30f0038b677556203
Getsumi3 <tomiuc117@gmail.com>
2018-07-09 17:19:16 +0300

    projects tab

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -52,3 +52,36 @@ jQuery(document).ready(function() {
   return false;
  })
 });
+
+var sections = ["fjSlides", "dcSlides", "apSlides", "rlSlides"];
+var intervals = new Array();
+var randoms = new Array();
+
+function restart(hsn, i) { 
+    intervals[i] = setInterval(function () {
+        var total = $(hsn + ' > div').length
+        var previous = randoms[i]
+        var random = Math.floor(Math.random() * total);
+        if (random == randoms[i]) {
+            random = (random + 1) % total 
+        }
+        randoms[i] = random
+        $(hsn + ' > div').eq(random)
+            .fadeIn(300);
+        $(hsn + ' > div').eq(previous)
+            .hide();
+    }, 2000);
+}
+
+function stop(i) {
+    clearInterval(intervals[i]);
+}
+
+$(function () {
+    for (var i = 0; i < sections.length; i++) {
+        var hash_section_name = '#' + sections[i];
+        $(hash_section_name + ' > div').hide();
+        restart(hash_section_name, i);
+    }
+});
+
