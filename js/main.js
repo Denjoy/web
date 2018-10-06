77f2c4e8f568e9f723a26da7ba9975782c2f2ff9
Denjoy <pokerdenj210695@gmail.com>
2018-10-06 16:05:44 +0200

    ScrollBar, Menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$("html").niceScroll({
+		cursorwidth:5,
+		cursorborder:0,
+		railpadding: { top: 20, right: 0, left: 0, bottom: 20 },
+		cursorcolor: 'gray',
+		autohidemode:true,
+	});
+
+});
